ffbfe585199543f04144697c7cec6a1a3ff915d0
Prime-DS <prime.up17@gmail.com>
2022-10-14 20:44:48 +0300

    add task

--- src/components/Feedback/Feedback .jsx
+++ src/components/Feedback/Feedback .jsx
@@ -1,56 +1,114 @@
-import React from "react";
+
 import { Statistics } from '../Statistics/Statistics';
 import { FeedbackOptions } from '../FeedbackOptions/FeedbackOptions';
-
 import {Wrapper} from './Feedback.styled'
-class Feedback extends React.Component {
-    state = {
-        good: 0,
-        neutral: 0,
-        bad: 0
-    };
+import { useState } from "react";
 
-  
-    CounterFeedback = (eve) => {
-        this.setState(prevState => {
-            return {
-                [eve]: prevState[eve] +1,
-            }
-        })
+export default function Feedback () {
+    const [good,setGood]= useState(0)
+    const [neutral,setNeutral]= useState(0)
+    const [bad,setBad]= useState(0)
+
+    const CounterFeedback = (item) => {
+        switch(item){
+            case "good":
+                return setGood((prev)=>prev+1);
+            case "neutral":
+                    return setNeutral((prev)=>prev+1);
+            case "bad":
+                    return setBad((prev)=>prev+1);
+                default:
+                    return;
+        }
     };
     
-     countTotalFeedback () {
-         return this.state.good + this.state.neutral + this.state.bad;
+     const countTotalFeedback = () => {
+         return good + neutral + bad;
     };
     
-    countPositiveFeedbackPercentage() {
-        const total = this.countTotalFeedback();
-        const good = this.state.good;
+    const countPositiveFeedbackPercentage =()=> {
+        const total = countTotalFeedback();
+        // const good = state.good;
         return Math.round((good / total) * 100);
     }
 
-    render() {
-        return (
+
+    const total=countTotalFeedback();
+    const positivePercentage=countPositiveFeedbackPercentage();
+    // const options = ['good', 'neutral', 'bad'];
+    return (
         <Wrapper>
             <h1>Please leave feedback</h1>
                 <FeedbackOptions
-                    options={Object.keys(this.state)}
-                    onLeaveFeedback={this.CounterFeedback}>
+                    options={['good', 'neutral', 'bad']}
+                    onLeaveFeedback={CounterFeedback}>
                 </FeedbackOptions>
-
                 <section>
                 <h2>Statistics</h2>
-                    {this.countTotalFeedback() ? <Statistics
-                        good={this.state.good}
-                        neutral={this.state.neutral}
-                        bad={this.state.bad}
-                        total={this.countTotalFeedback()}
-                        positivePercentage={this.countPositiveFeedbackPercentage()}></Statistics> : <h2>No feedback given</h2>
+                    {countTotalFeedback() ? <Statistics
+                        good={good}
+                        neutral={neutral}
+                        bad={bad}
+                        total={total}
+                        positivePercentage={positivePercentage}>
+
+                        </Statistics> : <h2>No feedback given</h2>
                 }
                 </section>
-            
-            
-        </Wrapper>)
-    };
+        </Wrapper>
+    )
 };
-export default Feedback;
+
+
+
+// class Feedback extends React.Component {
+//     state = {
+//         good: 0,
+//         neutral: 0,
+// //         bad: 0
+// //     };
+
+  
+//     CounterFeedback = (eve) => {
+//         this.setState(prevState => {
+//             return {
+//                 [eve]: prevState[eve] +1,
+//             }
+//         })
+//     };
+    
+//      countTotalFeedback () {
+//          return this.state.good + this.state.neutral + this.state.bad;
+//     };
+    
+//     countPositiveFeedbackPercentage() {
+//         const total = this.countTotalFeedback();
+//         const good = this.state.good;
+//         return Math.round((good / total) * 100);
+//     }
+
+//     render() {
+//         return (
+//         <Wrapper>
+//             <h1>Please leave feedback</h1>
+//                 <FeedbackOptions
+//                     options={Object.keys(this.state)}
+//                     onLeaveFeedback={this.CounterFeedback}>
+//                 </FeedbackOptions>
+
+//                 <section>
+//                 <h2>Statistics</h2>
+//                     {this.countTotalFeedback() ? <Statistics
+//                         good={this.state.good}
+//                         neutral={this.state.neutral}
+//                         bad={this.state.bad}
+//                         total={this.countTotalFeedback()}
+//                         positivePercentage={this.countPositiveFeedbackPercentage()}></Statistics> : <h2>No feedback given</h2>
+//                 }
+//                 </section>
+            
+            
+//         </Wrapper>)
+//     };
+// };
+// export default Feedback;
